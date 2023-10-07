36b1986f0feca99a69135d5923dfbe5f318ea4b8
ryota-sb <ryota.sb.1206@gmail.com>
2023-10-08 00:10:46 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :customers, controllers: {
+    sessions: 'customer/sessions',
+    registrations: 'customer/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
   root to: 'pages#home'
 
   get '/up/', to: 'up#index', as: :up
